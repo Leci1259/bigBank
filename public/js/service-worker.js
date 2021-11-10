256dba5e13b0231895c6c3b822e2de5a75e945b2
Leci1259 <lreid31@gatech.edu>
2021-11-10 14:14:09 -0500

    added more constants

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,10 +1,13 @@
 const FILES_TO_CACHE = [
+    "/",
     "../icons/icon-192x192.png",
     "../icons/icon-512x512.png",
     "../css/styles.css",
     "../html/index.html",
+    "../manifest.json",
     "./index.js",
     "./db.js",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 const CACHE_NAME = "static-cache-v2";
