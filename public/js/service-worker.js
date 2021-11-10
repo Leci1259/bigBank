9d01e7a8ae25c5363c06c10f54592a4bea8cbb47
Leci1259 <lreid31@gatech.edu>
2021-11-10 14:12:26 -0500

    inserted constants for caching setup

---
+++ public/js/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    "../icons/icon-192x192.png",
+    "../icons/icon-512x512.png",
+    "../css/styles.css",
+    "../html/index.html",
+    "./index.js",
+    "./db.js",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
