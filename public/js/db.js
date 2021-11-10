eeec65ba1dbc7266ce0c3500ad9ecc5031d67a40
Leci1259 <lreid31@gatech.edu>
2021-11-10 15:01:32 -0500

    added in database grab and post function; added eventlistener to check if online

--- public/js/db.js
+++ public/js/db.js
@@ -3,7 +3,7 @@ const request = window.indexedDB.open("budget", 1);
 request.onupgradeneeded = event => {
     const db = event.target.result;
 
-    const BudgetStore = db.createObjectStore("budget", {
+    const BudgetStore = db.createObjectStore("budgetList", {
         keyPath: "listID"
     });
 }
@@ -14,5 +14,42 @@ request.onsuccess = () => {
     // if online check db and send post it to site
     if (navigator.online) {
         //send to function to read through database and post
+        databaseCheck();
     }
 }
+
+function databaseCheck() {
+    //grab database and store with right permissions
+    const transaction = db.transaction(["budgetList"], "readwrite");
+    const BudgetStore = transaction.objectStore("budgetList");
+
+    //grab all data in db
+    const data = BudgetStore.getAll();
+
+    data.onsuccess = () => {
+        //if not empty
+        if (data.result.length > 0) {
+            //post data
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(data.result),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                //return data to screen
+                .then((response) => {
+                    return response.json();
+                })
+                //delete posted data out of cache
+                .then(() => {
+                    const transaction = db.transaction(["budgetList"], "readwrite");
+                    const BudgetStore = transaction.objectStore("budgetList");
+                    BudgetStore.clear();
+                });
+        };
+    };
+};
+
+//anytime site comes online automatically check & post
+window.addEventListener("online", databaseCheck);
